f79484e1f9d1137b5847da3ab02ecf8b655f62ca
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-18 13:52:50 -0700

    convert to es2015 with lebab.sh

--- 02/vis.js
+++ 02/vis.js
@@ -2,28 +2,28 @@
     // This function simulates fetching with a 2 second delay.
     // You can replace stuff here with e.g. d3.csv.
     function fetchData(callback){
-      setTimeout(function (){
+      setTimeout(() => {
         callback([6, 5, 4, 3, 2, 1]);
       }, 2000);
     }
     
     // This function visualizes the data.
     function visualize(selection, data){
-      var rects = selection
+      const rects = selection
         .selectAll("rect")
         .data(data);
       rects.exit().remove();
       rects
         .enter().append("rect")
-          .attr("x", function (d, i){ return i * 100 + 182; })
-          .attr("y", function (d){ return 400; })
+          .attr("x", (d, i) => i * 100 + 182)
+          .attr("y", d => 400)
           .attr("width", 50)
           .attr("height", 0)
         .merge(rects)
         .transition().duration(1000).ease(d3.easeBounce)
-          .delay(function (d, i){ return i * 500; })
-          .attr("y", function (d){ return 400 - d * 50; })
-          .attr("height", function (d){ return d * 50; });
+          .delay((d, i) => i * 500)
+          .attr("y", d => 400 - d * 50)
+          .attr("height", d => d * 50);
     }
  
     // The stuff below uses d3-component to display a spinner
@@ -31,55 +31,58 @@
     
     // This stateless component renders a static "wheel" made of circles,
     // and rotates it depending on the value of props.angle.
-    var wheel = d3.component("g")
+    const wheel = d3.component("g")
       .create(function (){
-        var minRadius = 4,
-            maxRadius = 10,
-            numDots = 10,
-            wheelRadius = 40,
-            rotation = 0,
-            rotationIncrement = 3, 
-            radius = d3.scaleLinear()
-              .domain([0, numDots - 1])
-              .range([maxRadius, minRadius]),
-            angle = d3.scaleLinear()
-              .domain([0, numDots])
-              .range([0, Math.PI * 2]);
-        d3.select(this)
-          .selectAll("circle").data(d3.range(numDots))
-          .enter().append("circle")
-            .attr("cx", function (d){ return Math.sin(angle(d)) * wheelRadius; })
-            .attr("cy", function (d){ return Math.cos(angle(d)) * wheelRadius; })
-            .attr("r", radius);
-      })
+      const minRadius = 4;
+      const maxRadius = 10;
+      const numDots = 10;
+      const wheelRadius = 40;
+      const rotation = 0;
+      const rotationIncrement = 3;
+
+      const radius = d3.scaleLinear()
+        .domain([0, numDots - 1])
+        .range([maxRadius, minRadius]);
+
+      const angle = d3.scaleLinear()
+        .domain([0, numDots])
+        .range([0, Math.PI * 2]);
+
+      d3.select(this)
+        .selectAll("circle").data(d3.range(numDots))
+        .enter().append("circle")
+          .attr("cx", d => Math.sin(angle(d)) * wheelRadius)
+          .attr("cy", d => Math.cos(angle(d)) * wheelRadius)
+          .attr("r", radius);
+    })
       .render(function (d){
-        d3.select(this).attr("transform", "rotate(" + d + ")");
+        d3.select(this).attr("transform", `rotate(${d})`);
       });
     
     // This component with a local timer makes the wheel spin.
-    var spinner = (function (){
-      var timer = d3.local();
+    const spinner = ((() => {
+      const timer = d3.local();
       return d3.component("g")
         .create(function (d){
-          timer.set(this, d3.timer(function (elapsed){
+          timer.set(this, d3.timer(elapsed => {
             d3.select(this).call(wheel, elapsed * d.speed);
-          }.bind(this)));
+          }));
         })
         .render(function (d){
-          d3.select(this).attr("transform", "translate(" + d.x + "," + d.y + ")");
+          d3.select(this).attr("transform", `translate(${d.x},${d.y})`);
         })
         .destroy(function(d){
           timer.get(this).stop();
           return d3.select(this)
               .attr("fill-opacity", 1)
             .transition().duration(3000)
-              .attr("transform", "translate(" + d.x + "," + d.y + ") scale(10)")
+              .attr("transform", `translate(${d.x},${d.y}) scale(10)`)
               .attr("fill-opacity", 0);
         });
-    }());
+    })());
     
     // This component displays the visualization.
-    var visualization = d3.component("g")
+    const visualization = d3.component("g")
       .render(function (d){
         d3.select(this).call(visualize, d.data);
       });
@@ -87,7 +90,7 @@
     // This component manages an svg element, and
     // either displays a spinner or text,
     // depending on the value of the `loading` state.
-    var app = d3.component("g")
+    const app = d3.component("g")
       .render(function (d){
         d3.select(this)
             .call(spinner, !d.loading ? [] : {
@@ -100,24 +103,24 @@
     
     // Kick off the app.
     function main(){
-      var svg = d3.select("svg"),
-          width = svg.attr("width"),
-          height = svg.attr("height");
+      const svg = d3.select("svg");
+      const width = svg.attr("width");
+      const height = svg.attr("height");
 
       // Initialize the app to be "loading".
       svg.call(app, {
-        width: width,
-        height: height,
+        width,
+        height,
         loading: true
       });
 
       // Invoke the data fetching logic.
-      fetchData(function (data){
+      fetchData(data => {
         svg.call(app, {
-          width: width,
-          height: height,
+          width,
+          height,
           loading: false,
-          data: data
+          data
         });
       });
     }
